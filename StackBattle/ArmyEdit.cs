6cc61a4ab14f4ff97d08c6134ae4948fad413080
EdieLay <98664652+EdieLay@users.noreply.github.com>
2023-05-17 14:01:49 +0300

    Улучшения в GUI формы редактирования армии

--- StackBattle/ArmyEdit.cs
+++ StackBattle/ArmyEdit.cs
@@ -22,7 +22,41 @@ namespace StackBattle
             Battle battle = Battle.GetBattleInstance();
             label_armynum.Text = battle.IsFirstArmyBeingEdited ? "Army №1" : "Army №2";
             Army army = battle.GetArmy();
-            
+            comboBox_unitTypeSelection.SelectedItem = comboBox_unitTypeSelection.Items[0];
+
+            GetUnitsString(army);
+            if (army.ArmySize != 0)
+                comboBox_armyUnitSelection.SelectedItem = comboBox_armyUnitSelection.Items[0];
+        }
+
+        private void comboBox_unitTypeSelection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int unitType = comboBox_unitTypeSelection.SelectedIndex;
+            if (unitType < 3)
+                HideSA(true);
+            else HideSA(false);
+        }
+
+        private void HideSA(bool isNotSAUnit) // скрытие ненужных характеристик для юнитов без спешл абилити
+        {
+            if (isNotSAUnit)
+            {
+                label_SAR.Hide();
+                label_SAS.Hide();
+                numericUpDown_sar.Hide();
+                numericUpDown_sas.Hide();
+            }
+            else
+            {
+                label_SAR.Show();
+                label_SAS.Show();
+                numericUpDown_sar.Show();
+                numericUpDown_sas.Show();
+            }
+        }
+
+        void GetUnitsString(Army army)
+        {
             for (int i = 0; i < army.ArmySize; i++)
             {
                 string test = $"{i} {army[i].GetType()} {army[i].HitPoints}/{army[i].Attack}/{army[i].Defense}";
