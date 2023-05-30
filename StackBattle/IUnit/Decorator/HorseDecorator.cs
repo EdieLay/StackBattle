2c1a51524ecdd74fc6eed40bcc6ad322dc7eb659
Анастасия <onastja02@gmail.com>
2023-05-30 20:03:38 +0300

    AbsractDecorator и его конкретные реализации

---
+++ StackBattle/IUnit/Decorator/HorseDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackBattle
+{
+    internal class HorseDecorator : AbstractDecorator
+    {
+        public HorseDecorator(IUnit component) : base(component)
+        { }
+        public new int Attack
+        {
+            get
+            {
+                return _component.Attack + 5;
+            }
+        }
+    }
+}
