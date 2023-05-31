2468869bc1dd09b5ae49948bc2361ab6b2352271
Анастасия <onastja02@gmail.com>
2023-05-31 21:58:41 +0300

    LineStructure

--- StackBattle/ArmyStructure/LineStructure.cs
+++ StackBattle/ArmyStructure/LineStructure.cs
@@ -10,7 +10,78 @@ namespace StackBattle
     {
         public bool DoTurn(Army firstArmy, Army secondArmy)
         {
-            throw new NotImplementedException();
+            if (firstArmy.Units.Count == 0 || secondArmy.Units.Count == 0)
+                return false;
+
+            int maxArmyLength = Math.Max(firstArmy.ArmySize, secondArmy.ArmySize);
+            for (int i = 0; i < maxArmyLength; i++)
+            {
+                if (i < firstArmy.ArmySize)
+                {
+                    if (firstArmy[i] is IDressBuff dressBuff)
+                    {
+                        ArmiesRange armies = new(firstArmy, secondArmy);
+                        GetAreasInRange(i, 1, armies);
+                        dressBuff.DressBuff(armies);
+                    }
+                }
+                if (i < secondArmy.ArmySize)
+                {
+                    if (secondArmy[i] is IDressBuff dressBuff)
+                    {
+                        ArmiesRange armies = new(secondArmy, firstArmy);
+                        GetAreasInRange(i, 1, armies);
+                        dressBuff.DressBuff(armies);
+                    }
+                }
+            }
+
+            int minArmyLength = Math.Min(firstArmy.ArmySize, secondArmy.ArmySize);
+            for (int i = 0; i < minArmyLength; i++)
+            {
+                secondArmy[i].TakeDamage(firstArmy[i].Attack);
+                Battle.TakeOffBuff(ref firstArmy[i]);
+                firstArmy[i].TakeDamage(secondArmy[i].Attack);
+                Battle.TakeOffBuff(ref secondArmy[i]);
+            }
+
+            
+            for (int i = minArmyLength; i < maxArmyLength; i++)
+            {
+                if (i < firstArmy.ArmySize)
+                {
+                    if (firstArmy[i] is ISpecialAbility saunit)
+                    {
+                        ArmiesRange armies = new(firstArmy, secondArmy);
+                        GetAreasInRange(i, saunit.Range, armies);
+                        saunit.Action(armies);
+                    }
+                }
+                if (i < secondArmy.ArmySize)
+                {
+                    if (secondArmy[i] is ISpecialAbility saunit)
+                    {
+                        ArmiesRange armies = new(secondArmy, firstArmy);
+                        GetAreasInRange(i, saunit.Range, armies);
+                        saunit.Action(armies);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void GetAreasInRange(int position, int range, ArmiesRange armies)
+        {
+            int armyStartInRange = position - range < 0 ? 0 : position - range;
+            int armyEndInRange = position + range >= armies.friendlyArmy.ArmySize ? armies.friendlyArmy.ArmySize - 1 : position + range;
+            for (int i = armyStartInRange; i < position; i++)
+                armies.fArea.Add(i);
+            for (int i = position + 1; i <= armyEndInRange; i++)
+                armies.fArea.Add(i);
+
+            for (int i = armyStartInRange; i < armies.enemyArmy.ArmySize; i++)
+                armies.eArea.Add(i);
         }
     }
 }
