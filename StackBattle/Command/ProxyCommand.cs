16c1ae84ad0f30910b5f699cefc796c5ccc67fd7
Анастасия <onastja02@gmail.com>
2023-06-05 12:06:10 +0300

    добавлен класс ProxyCommand

---
+++ StackBattle/Command/ProxyCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StackBattle
+{
+    internal class ProxyCommand : ICommand
+    {
+        private ICommand _command;
+        public string LogFile { get; private set; } = "LogFile.txt";
+
+        public ProxyCommand(ICommand command)
+        {
+            _command = command;
+            using StreamWriter writer = new(LogFile, false);
+        }
+
+        public bool IsUndoAvailable { get { return _command.IsUndoAvailable; } }
+        public bool IsRedoAvailable { get { return _command.IsRedoAvailable; } }
+
+        public void Execute()
+        {
+            _command.Execute();
+        }
+        
+        public void Undo()
+        {
+            _command.Undo();
+        }
+
+        public void Redo()
+        {
+            _command.Redo();
+        }
+
+        public void SetStructure(ArmyStructure structure)
+        {
+            _command.SetStructure(structure);
+        }
+
+        public void Log()
+        {
+            using StreamWriter writer = new(LogFile, true);
+            writer.Write(DateTime.Now);
+        }
+    }
+}
