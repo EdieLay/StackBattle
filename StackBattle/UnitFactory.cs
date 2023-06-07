2491d137f60c5d036a2fb6be20078cdd3045663e
EdieLay <98664652+EdieLay@users.noreply.github.com>
2023-06-07 18:06:15 +0300

    yes

--- StackBattle/UnitFactory.cs
+++ StackBattle/UnitFactory.cs
@@ -101,7 +101,7 @@ namespace StackBattle
 
         public void CreateRandomArmy(int price)
         {
-            army.Units.Clear(); // no
+            army.Units.Clear();
 
             int ratio = Math.Max(1, price / 50);
             int priceLeft = price;
